c6c16af4394ee04734511f8ccf0b9999d9109d02
PiyushNegi1703 <112609875+PiyushNegi1703@users.noreply.github.com>
2022-12-12 10:54:16 +0530

    3 files modified

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-// create a function and following the progressions inside the function.
+let promiseCall = new Promise((resolve, reject) => {
+  resolve(fetch('https://jsonplaceholder.typicode.com/users'));
+});
 
-// Progression 1: Create a promise call which fetches data
-// Progression 2: Display the fetched data in the form of list
-// Progression 3: When the promise call is rejected then throw an error
+const message = document.getElementById('message');
+const h2Text = document.querySelector('h2');
+const button = document.getElementById('btnGet');
+
+message.style.display = 'none';
+h2Text.style.display = 'none';
+
+promiseCall
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((resp) => {
+    console.log(resp);
+    var player = ``;
+
+    resp.forEach((user) => {
+      player += `<div class="player">
+          <div class="strength">Name : ${user.name}</div>
+          <div>Email   : ${user.email}</div>
+          <div>Phone   : ${user.phone}</div>
+          <div>Website : ${user.website}</div>
+          <div>Company : ${user.company.name}</div>
+          <div>City    : ${user.address.city}</div>
+          <div>Zipcode : ${user.address.zipcode}</div>
+          </div>`;
+    });
+
+    message.innerHTML = player;
+  });
+
+button.onclick = () => {
+  if (message.style.display == 'none') {
+    message.style.display = 'grid';
+    button.innerText = 'Hide User List';
+    h2Text.style.display = 'inherit';
+  } else if (message.style.display == 'grid') {
+    message.style.display = 'none';
+    button.innerText = 'Get User List';
+    h2Text.style.display = 'none';
+  }
+};
+
+promiseCall.catch((error) => {
+  console.log('Promise rejected.');
+  console.log(error.message);
+});
